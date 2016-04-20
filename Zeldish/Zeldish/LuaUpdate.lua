c5b6582b7a0767c369881db6f098982c4b54fc66
Oscar Engborg <Engborg@live.se>
2016-04-20 12:19:55 +0200

    Moved region to lua module

--- Zeldish/Zeldish/LuaUpdate.lua
+++ Zeldish/Zeldish/LuaUpdate.lua
@@ -16,8 +16,8 @@ function Update()
 		UpdateMenu()
 		DrawMenu()
 	elseif gameState == 1 then
-		UpdateRegion()
-		DrawRegion()
+		region.UpdateRegion()
+		region.DrawRegion()
 	end
 
 	return 1
@@ -34,7 +34,7 @@ function UpdateMenu ()
 		selected = menu:GetSelected()
 		if selected == 0 then
 			gameState = 1
-			CreateRegion()
+			region = require("Region")
 		elseif selected == 2 then
 			gameState = -1
 		end
@@ -47,103 +47,4 @@ end
 function DrawMenu ()
 	--Draw menu
 	menu:Draw()	
-end
-
-function UpdateRegion()
-	if Input.IsPressed(36) == 1 then
-		gameState = 0
-	end
-	if Input.IsPressed(57) == 1 then
-		LoadTileMap(2)
-	end
-end
-
-function LoadTileMap (level)
-	--Load TileMap
-	tileMapBackground = TileMap.New();
-	tileMapForeground = TileMap.New();
-
-	--tiles size in pixels
-	tileSizeX = 16
-	tileSizeY = 16
-
-	--amount of tiles in the map
-	sizeX = 32
-	sizeY = 32
-	
-	if level == 1 then
-		map = MapOneB()
-	elseif level == 2 then
-		map = MapTwoB()
-	end
-
-	tileMapBackground:Load("town_tiles.png", tileSizeX, tileSizeY, sizeX, sizeY, map);
-
-	if level == 1 then
-		map = MapOneF()
-	elseif level == 2 then
-		map = MapTwoF()
-	end
-
-	tileMapForeground:Load("town_tiles.png", tileSizeX, tileSizeY, sizeX, sizeY, map);
-
-end
-
-function DrawRegion ()
-	--Draw tileMap
-	tileMapBackground:Draw()
-	tileMapForeground:Draw()
-end
-
-function MapOneB()
-	map = {}
-
-	for i = 1, 32 * 32 do
-		map[i] = 11	-- all grass
-	end
-	
-	for j = 1, 32 * 16 do
-		map[j] = 7	-- all grass
-	end
-
-	return map
-end
-
-function MapTwoB()
-	map = {}
-
-	for i = 1, 32 * 32 do
-		map[i] = 11	-- all grass
-	end
-
-	return map
-end
-
-
-function MapOneF()
-	map = {}
-
-	for i = 1, 32 * 32 do
-		map[i] = -1	-- all grass
-	end
-	
-	map[34] = 13;
-
-	return map
-end
-
-function MapTwoF()
-	map = {}
-
-	for i = 1, 32 * 32 do
-		map[i] = -1	-- all grass
-	end
-
-	map[534] = 13;
-
-	return map
-end
-
-function CreateRegion()
-	LoadTileMap(1)
 end
