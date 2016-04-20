26108f10b68071a3564b2e29553cac618ffe1fbb
Pistonnoob <kimeld@hotmail.com>
2016-04-20 09:57:40 +0200

    Entity + Getters & Setters

--- Zeldish/Zeldish/Entity.cpp
+++ Zeldish/Zeldish/Entity.cpp
@@ -4,9 +4,75 @@
 
 Entity::Entity()
 {
+	this->x = this->y = this->width = this->height = 0;
+	this->boundingBox = BoundingVolume();
 }
 
 
 Entity::~Entity()
+{
+}
+
+int Entity::Initialize()
+{
+	int result = 0;
+
+	this->x = 0;
+	this->y = 0;
+	this->width = 32;
+	this->height = 32;
+
+	this->drawTexture.loadFromFile("../Zeldish/Resources/Textures/RacoonCharacter.png");
+	
+	return result;
+}
+
+void Entity::SetX(int x)
+{
+	this->x = x;
+}
+
+void Entity::SetY(int y)
+{
+	this->y = y;
+}
+
+void Entity::SetPos(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+void Entity::SetWidth(int width)
+{
+	this->width = width;
+}
+
+void Entity::SetHeight(int height)
+{
+	this->height = height;
+}
+
+int Entity::GetX()
+{
+	return this->x;
+}
+
+int Entity::GetY()
+{
+	return this->y;
+}
+
+int Entity::GetWidth()
+{
+	return this->width;
+}
+
+int Entity::GetHeight()
+{
+	return this->height;
+}
+
+void Entity::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
 }
--- Zeldish/Zeldish/Entity.h
+++ Zeldish/Zeldish/Entity.h
@@ -2,14 +2,30 @@
 #define ENTITY_H
 #include "BoundingVolume.h"
 #include "SFML\Graphics.hpp"
+#include "WindowLib.h"
 class Entity : public sf::Drawable , sf::Transformable
 {
 protected:
+	int x, y, width, height;
 
+	BoundingVolume boundingBox;
+	sf::Texture drawTexture;
+	sf::RectangleShape rect;
 public:
 	Entity();
 	virtual ~Entity();
+	virtual int Initialize();
 
+	void SetX(int x);
+	void SetY(int y);
+	void SetPos(int x, int y);
+	void SetWidth(int width);
+	void SetHeight(int height);
+
+	int GetX();
+	int GetY();
+	int GetWidth();
+	int GetHeight();
 
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
