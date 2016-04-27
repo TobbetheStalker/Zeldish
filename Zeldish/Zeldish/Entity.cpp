2f8c1b11b3d16f7442e993aeee356a4b6c4bc958
Pistonnoob <kimeld@hotmail.com>
2016-04-27 13:23:40 +0200

    Entity Intersection test added

--- Zeldish/Zeldish/Entity.cpp
+++ Zeldish/Zeldish/Entity.cpp
@@ -243,6 +243,15 @@ int Entity::UpdateSprite(float dTime)
 	return result;
 }
 
+int Entity::Intersects(Entity * other)
+{
+	int result = 0;
+
+	result = this->boundingBox.CheckAgainst(&other->boundingBox);
+
+	return result;
+}
+
 void Entity::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
 	target.draw(this->mySprite, states);
@@ -513,6 +522,20 @@ int entity_update(lua_State* ls)
 	return 0;
 }
 
+int entity_Intersects(lua_State* ls)
+{
+	Entity* entity = checkEntity(ls, 1);
+	Entity* other = checkEntity(ls, 2);
+	int result = 0;
+	if (entity)
+	{
+		result = entity->Intersects(other);
+	}
+	lua_pushinteger(ls, result);
+
+	return 1;
+}
+
 void RegisterEntity(lua_State * ls)
 {
 	// Create a luaL metatable. This metatable is not 
@@ -548,6 +571,7 @@ void RegisterEntity(lua_State * ls)
 		{ "GetDirection",	entity_getDirection },
 		{ "GetSpeed",		entity_getSpeed },
 		{ "Update",			entity_update },
+		{ "Intersects",		entity_Intersects },
 		{ "__gc",			entity_destroy },
 		{ NULL, NULL }
 	};
