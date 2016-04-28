d28f9398fb34dd31a42ab988ecb36b73e9373c61
Tobbe <tobbe.kling@hotmail.com>
2016-04-28 11:38:05 +0200

    Added collision to the ai

--- Zeldish/Zeldish/AI.lua
+++ Zeldish/Zeldish/AI.lua
@@ -7,13 +7,13 @@ end
 function ai.Update(player, entities)
 	
 	for i = 1, #entities do
-		ai.MoveEnemy(player, entities[i])
+		MoveEnemy(player, entities[i])
 
 	end
 
 end
 
-function ai.MoveEnemy(player, enemy)
+function MoveEnemy(player, enemy)
 	enemyX, enemyY = enemy:GetPos();
 	playerX, playerY = player:GetPos()
 
@@ -24,6 +24,7 @@ function ai.MoveEnemy(player, enemy)
 
 	if difX == 0 and difY == 0 then
 		enemy:SetDirection(4)
+
 	elseif math.abs(difX) > math.abs(difY) then -- If X is dominant
 	
 		if difX < 0 then	--Right
@@ -42,5 +43,38 @@ function ai.MoveEnemy(player, enemy)
 	end
 
 end 
+
+function PlayerEnemyCollision(player, enemy)
+
+	enemyX, enemyY = enemy:GetPos();
+	playerX, playerY = player:GetPos()
+
+	difX = enemyX - playerX
+	difY = enemyY - playerY
+
+	if difX <= 20 and difY <= 20 then
+		
+		if enemy:Intersects(player) then
+			
+			if math.abs(difX) < math.abs(difY) then	--X is smaller
+				
+				if difX < 0 then
+					enemy:SetDirection(2)
+				else
+					enemy:SetDirection(1)
+				end
+			else
+				if difY < 0 then	-- Y is smaller
+					enemy:SetDirection(0)	-- Down
+				else
+					enemy:SetDirection(3) --Up
+				end
+			end
+		end		
+	end
+
+
+end
+
 
 return ai
